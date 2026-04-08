a1f8e290d657b2661d861139040a61d58731d620
Ajay Kumar <158462635+Ajaybhukya@users.noreply.github.com>
2026-04-08 15:31:31 +0530

    Add buildPipeline function for Jenkins pipeline

---
+++ jenkins-shared-lib/vars/buildPipeline.groovy
@@ -0,0 +1,58 @@
+def call(Map config) {
+
+    pipeline {
+        agent any
+
+        stages {
+
+            stage('Load Environment') {
+                steps {
+                    loadEnv(config.envCredential)
+                }
+            }
+
+            stage('Checkout') {
+                steps {
+                    git branch: config.branch,
+                        url: config.repoUrl
+                }
+            }
+
+            stage('Compile') {
+                steps {
+                    sh "mvn clean compile -DskipTests"
+                }
+            }
+
+            stage('Parallel Checks') {
+                parallel {
+
+                    stage('Unit Tests') {
+                        steps {
+                            sh "mvn test"
+                        }
+                        post {
+                            always {
+                                junit 'target/surefire-reports/*.xml'
+                            }
+                        }
+                    }
+
+                    stage('SonarQube') {
+                        steps {
+                            sonarScan(config.sonarProjectKey)
+                        }
+                    }
+                }
+            }
+
+            stage('Quality Gate') {
+                steps {
+                    timeout(time: 2, unit: 'MINUTES') {
+                        waitForQualityGate abortPipeline: true
+                    }
+                }
+            }
+        }
+    }
+}
