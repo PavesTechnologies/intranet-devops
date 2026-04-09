78105d5b28fc97766788dc1779d0db687d6dbcb5
Ajay Kumar <158462635+Ajaybhukya@users.noreply.github.com>
2026-04-09 18:35:08 +0530

    Update qualityGateStage.groovy

--- jenkins-shared-lib/vars/qualityGateStage.groovy
+++ jenkins-shared-lib/vars/qualityGateStage.groovy
@@ -2,7 +2,8 @@
 def call() {
     // REMOVED 'steps' block
     // Wrapping in timeout directly
-    timeout(time: 2, unit: 'MINUTES') {
-        waitForQualityGate abortPipeline: true
-    }
+    // timeout(time: 2, unit: 'MINUTES') {
+    //     waitForQualityGate abortPipeline: true
+    // }
+    echo "Skip Quality Gate"
 }
