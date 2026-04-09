b55a66ee5a91ca4ec42ae000571cdba1db22e982
Ajay Kumar <158462635+Ajaybhukya@users.noreply.github.com>
2026-04-09 15:57:34 +0530

    Refactor buildPipeline.groovy for cleaner syntax

--- jenkins-shared-lib/vars/buildPipeline.groovy
+++ jenkins-shared-lib/vars/buildPipeline.groovy
@@ -1,69 +1,46 @@
 def call(Map config) {
-
     pipeline {
         agent any
-
         stages {
-
             stage('Initialization') {
                 steps {
-                    script {
-                        initStages(config)
-                    }
+                    script { initStages(config) }
                 }
             }
-            
-             stage('Setup Java') {
+            stage('Setup Java') {
                 steps {
                     script {
                         def selectedJdk = config.jdk ?: 'jdk17'
-
                         echo "Using JDK: ${selectedJdk}"
-
                         env.JAVA_HOME = tool selectedJdk
                         env.PATH = "${env.JAVA_HOME}/bin:${env.PATH}"
-
                         sh 'java -version'
                     }
                 }
             }
-            
             stage('Compile') {
                 steps {
                     sh "mvn clean compile -DskipTests"
                 }
             }
-
             stage('Parallel Checks') {
                 parallel {
-
                     stage('Unit Tests') {
-                        steps {
-                            sh "mvn test"
-                        }
+                        steps { sh "mvn test" }
                         post {
-                            always {
-                                junit 'target/surefire-reports/*.xml'
-                            }
+                            always { junit 'target/surefire-reports/*.xml' }
                         }
                     }
-
                     stage('SonarQube') {
-                        steps {
-                            sonarScan(config.sonarProjectKey)
-                        }
+                        steps { sonarScan(config.sonarProjectKey) }
                     }
                 }
             }
-
             stage('Quality Gate') {
                 steps {
-                    script {
-                        qualityGateStage()
-                    }
-                    }
+                    script { qualityGateStage() }
                 }
             }
-        }
-    }
-}
+        } // End stages
+    } // End pipeline
+} // End call
