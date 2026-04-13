1462cfcc90ddccdc021eb1ee385b0b2c8309e2fa
Ajay Kumar <158462635+Ajaybhukya@users.noreply.github.com>
2026-04-13 12:07:35 +0530

    Modify unit tests stage in buildPipeline.groovy
    Updated unit test stage to run 'mvn test' and added JUnit report publishing.

--- jenkins-shared-lib/vars/buildPipeline.groovy
+++ jenkins-shared-lib/vars/buildPipeline.groovy
@@ -25,9 +25,13 @@ def call(Map config) {
             }
             // stage('Parallel Checks') {
             //     parallel {
-                    stage('Unit Tests') {
-                        steps { sh "mvn clean test -B -e" }
+                     stage('Unit Tests') {
+                        steps { sh "mvn test" }
+                        post {
+                            always { junit 'target/surefire-reports/*.xml' }
+                        }
                     }
+            
                     stage('SonarQube') {
                         steps { sonarScan(config.sonarProjectKey) }
                     }
