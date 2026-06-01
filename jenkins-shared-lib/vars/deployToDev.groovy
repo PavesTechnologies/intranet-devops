3a695ee23311af206bdc29b3fa8f50b1d1625599
Ajay Kumar <158462635+Ajaybhukya@users.noreply.github.com>
2026-06-01 17:17:19 +0530

    Change GitHub credentials ID in deployToDev.groovy

--- jenkins-shared-lib/vars/deployToDev.groovy
+++ jenkins-shared-lib/vars/deployToDev.groovy
@@ -115,7 +115,7 @@ def call(Map config) {
         steps {
           withCredentials([
             usernamePassword(
-              credentialsId: 'github-pat',
+              credentialsId: 'all-cred',
               usernameVariable: 'GIT_USER',
               passwordVariable: 'GIT_TOKEN'
             )
